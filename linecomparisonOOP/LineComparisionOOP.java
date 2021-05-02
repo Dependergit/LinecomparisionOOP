2f3e9d7c30e440b7e5339760bb558a88815d9183
Your Name <you@example.com>
2021-05-03 00:54:52 +0530

    display welcome message

---
+++ linecomparisonOOP/LineComparisionOOP.java
@@ -0,0 +1,8 @@
+package com.company;
+import java.util.Scanner;
+public class LineComparisionOOP
+{
+    public static void main(String []args)
+    {   
+        System.out.println("Welcome to linecomparisionOOP program");
+}

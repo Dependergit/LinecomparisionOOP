15b43143f79b25027ecd4089c0a13a4c8b1f3bcd
Your Name <you@example.com>
2021-05-03 01:01:08 +0530

    calculating length of lines

--- linecomparisonOOP/LineComparisionOOP.java
+++ linecomparisonOOP/LineComparisionOOP.java
@@ -5,4 +5,31 @@ public class LineComparisionOOP
     public static void main(String []args)
     {   
         System.out.println("Welcome to linecomparisionOOP program");
+	 forlinefunctionsoop obj1 = new forlinefunctionsoop();
+         obj1.functionforlines();
 }
+class forlinefunctionsoop
+{
+    double length1;
+    double length2;
+    public void functionforlines()
+    {
+        double x1, y1, x2, y2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the cordinates for first line");
+        x1=sc.nextDouble();
+        y1=sc.nextDouble();
+        x2=sc.nextDouble();
+        y2=sc.nextDouble();
+         length1 = (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+        System.out.println(length1);
+
+        System.out.println("enter the cordinates for second line");
+        x1=sc.nextDouble();
+        y1=sc.nextDouble();
+        x2=sc.nextDouble();
+        y2=sc.nextDouble();
+         length2 = (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+        System.out.println(length2);
+
+    }
